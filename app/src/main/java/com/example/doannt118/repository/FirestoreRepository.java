214bcb95e7045bbfd0467efadaac25a8e7679852
UIT.23521221 <luka123lux@gmail.com>
2025-10-20 20:43:31 +0700

    commit changes

--- app/src/main/java/com/example/doannt118/repository/FirestoreRepository.java
+++ app/src/main/java/com/example/doannt118/repository/FirestoreRepository.java
@@ -3,8 +3,6 @@ package com.example.doannt118.repository;
 import com.example.doannt118.model.TaiKhoan;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.android.gms.tasks.Task;
-import java.util.List;
-
 public class FirestoreRepository {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
